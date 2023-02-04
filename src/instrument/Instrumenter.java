b40606939ae42a1c0d44be7edd63a46dd079f838
Mathias Wöß <mat.woess@gmail.com>
2023-02-04 21:14:17 +0100

    - introduce logic to find blocks into ATG  - associate blocks to class names  - return list of all blocks from parser

--- src/instrument/Instrumenter.java
+++ src/instrument/Instrumenter.java
@@ -1,5 +1,7 @@
 package instrument;
 
+import java.util.List;
+
 import static java.lang.System.exit;
 
 public class Instrumenter {
@@ -9,11 +11,13 @@ public class Instrumenter {
         "TestProgram.java",
     };
     for (String inputFile : inputFiles) {
-      instrumentFile(inputFile);
+      List<Parser.Block> blocks = analyzeFile(inputFile);
+      System.out.println("\n\nFound blocks:");
+      blocks.forEach(System.out::println);
     }
   }
 
-  private static void instrumentFile(String inputFile) {
+  private static List<Parser.Block> analyzeFile(String inputFile) {
     System.out.println("Reading File: \"" + inputFile + "\"");
     Parser parser = new Parser(new Scanner(inputFile));
     parser.Parse();
@@ -24,5 +28,6 @@ public class Instrumenter {
       System.out.println("aborting...");
       exit(1);
     }
+    return parser.allBlocks;
   }
 }
