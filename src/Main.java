9d09c36cc5dce3c7d614228d9fb1b01bc41b8c56
Mathias Wöß <mat.woess@gmail.com>
2023-02-11 21:36:58 +0100

    - implement exporting of blocks into .meta file - use index for __Counter.inc(), not beginning of block

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,14 @@ public class Main {
 
   public static void main(String[] args) {
     String[] inputFiles = new String[]{
-         "sample/Simple.java",
+         "sample/Classes.java",
     };
     for (String inputFile : inputFiles) {
       Instrumenter instrumenter = new Instrumenter();
       instrumenter.analyzeFile(inputFile);
       List<Parser.Block> blocks = instrumenter.getFoundBlocks();
       instrumenter.instrument();
+      instrumenter.exportBlockData();
       System.out.println("\n\nFound blocks:");
       blocks.forEach(System.out::println);
     }
--- src/instrument/Instrumenter.java
+++ src/instrument/Instrumenter.java
@@ -1,8 +1,9 @@
 package instrument;
 
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
@@ -11,6 +12,7 @@ import static java.lang.System.exit;
 public class Instrumenter {
   List<Parser.Block> foundBlocks;
   Path sourceFile;
+  Path targetFile = Path.of("out/profiler/Classes.java");;
 
   public void analyzeFile(String inputFile) {
     System.out.println("Reading File: \"" + inputFile + "\"");
@@ -32,19 +34,20 @@ public class Instrumenter {
   }
 
   public Path instrument() {
-    Path targetPath = Path.of("out/instrumented.java");
 //    try {
 //      Files.copy(sourceFile, targetPath, REPLACE_EXISTING);
 //    } catch (IOException ex) {
 //      System.out.println(ex);
 //    }
     try {
+      Path.of("out/profiler").toFile().mkdirs();
       String fileContent = Files.readString(sourceFile);
       System.out.println(fileContent);
       StringBuilder builder = new StringBuilder();
       int prevIdx = 0;
       Stack<Parser.Block> blockStack = new Stack<>();
-      for (Parser.Block block : foundBlocks) {
+      for (int i = 0; i < foundBlocks.size(); i++) {
+        Parser.Block block = foundBlocks.get(i);
         if (!blockStack.empty() && blockStack.peek().endPos < block.begPos) {
           Parser.Block prevBlock = blockStack.pop();
           builder.append(fileContent.substring(prevIdx, prevBlock.endPos));
@@ -58,7 +61,7 @@ public class Instrumenter {
         builder.append(fileContent.substring(prevIdx, block.begPos));
         prevIdx = block.begPos;
         if (block.insertBraces) builder.append('{');
-        builder.append(String.format("__Counter.inc(%d);", block.beg));
+        builder.append(String.format("__Counter.inc(%d);", i));
       }
       while (!blockStack.empty()) {
         Parser.Block prevBlock = blockStack.pop();
@@ -72,10 +75,71 @@ public class Instrumenter {
         }
       }
       builder.append(fileContent.substring(prevIdx));
-      Files.writeString(targetPath, builder.toString());
+      Files.writeString(targetFile, builder.toString());
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
-    return targetPath;
+    return targetFile;
+  }
+
+  public void exportBlockData() {
+    List<Class> classes = new ArrayList<>();
+    for (Parser.Block block : foundBlocks) {
+      Class clazz;
+      if (classes.isEmpty() || !classes.get(classes.size() - 1).name.equals(block.clazz)) {
+        clazz = new Class();
+        clazz.name = block.clazz;
+        classes.add(clazz);
+      } else {
+        clazz = classes.get(classes.size()-1);
+      }
+      Method meth;
+      if (clazz.methods.isEmpty() || !clazz.methods.get(clazz.methods.size() - 1).name.equals(block.method)) {
+        meth = new Method();
+        meth.name = block.method;
+        clazz.methods.add(meth);
+      } else {
+        meth = clazz.methods.get(classes.size()-1);
+      }
+      Block newBlock = new Block();
+      newBlock.beg = block.begPos;
+      newBlock.end = block.endPos;
+      newBlock.isMethodBlock = block.isMethodBlock;
+      meth.blocks.add(newBlock);
+    }
+    StringBuilder builder = new StringBuilder();
+    builder.append(foundBlocks.size()).append(" ");
+    for (Class clazz : classes) {
+      builder.append(clazz.name).append(" ");
+      for (Method meth: clazz.methods) {
+        builder.append(meth.name).append(" ");
+        for (Block block : meth.blocks) {
+          builder.append(block.beg).append(" ");
+          builder.append(block.end).append(" ");
+          builder.append(block.isMethodBlock ? 1 : 0).append(" ");
+        }
+      }
+      builder.append("#").append(" ");
+    }
+    Path metaDataFile = Path.of(targetFile.toString() + ".meta");
+    try (FileOutputStream fos = new FileOutputStream(metaDataFile.toFile())) {
+      byte[] data = builder.toString().getBytes();
+      fos.write(data, 0, data.length);
+      fos.flush();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+  class Class {
+    String name;
+    List<Method> methods = new ArrayList<>();
+  }
+  class Method {
+    String name;
+    List<Block> blocks = new ArrayList<>();
+  }
+  class Block {
+    int beg, end;
+    boolean isMethodBlock;
   }
 }
