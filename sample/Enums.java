e3f0ca848a8715f105a587477e8419c30364eac2
Mathias Wöß <mat.woess@gmail.com>
2023-03-02 21:07:38 +0100

    - support enums in grammar - add sample file for enums

---
+++ sample/Enums.java
@@ -0,0 +1,67 @@
+enum Adjective {
+  LOW, MEDIUM, HIGH,
+  WEAK, STRONG, GREAT
+}
+
+enum Empty {
+}
+
+enum Skill {
+  SINGING, PLAY_INSTRUMENT, ORGANIZING;
+
+  public String lowercase() {
+    return this.name().toLowerCase();
+  }
+}
+
+
+public enum Enums {
+  PRIMARY(Skill.SINGING, Adjective.GREAT, 1.5f),
+  SECONDARY(Skill.PLAY_INSTRUMENT, Adjective.MEDIUM, 5e+4f),
+  TERTIARY(Skill.ORGANIZING, Adjective.WEAK, .3f);
+
+  private final Skill skill;
+  private final Adjective adjective;
+  public final float number;
+
+  Enums(Skill skill, Adjective adj, float number) {
+    this.skill = skill;
+    this.adjective = adj;
+    this.number = number;
+  }
+
+  public static final long ID;
+
+  static {
+    ID = 5L;
+  }
+
+  public Skill getSkill() {
+    return skill;
+  }
+
+  private Adjective getAdjective() {
+    return adjective;
+  }
+
+  private synchronized void printSummary() {
+    if (adjective != null) {
+      if (skill != null) {
+        System.out.println(getAdjective().name() + " at " + getSkill().lowercase() + ", number: " + number);
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    Enums e = PRIMARY;
+    switch (e.getSkill()) {
+      case SINGING:
+        assert (e.getAdjective() == Adjective.GREAT);
+      default:
+        e.printSummary();
+    }
+    e = SECONDARY;
+    e.printSummary();
+    System.out.println(e.getSkill().lowercase());
+  }
+}
