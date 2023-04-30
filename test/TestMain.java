ec74c17a0c3f393b4dd511610be098fe9c6b0807
Mathias Wöß <mat.woess@gmail.com>
2023-04-30 02:24:10 +0200

    add test calling main method on all samples

---
+++ test/TestMain.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+public class TestMain {
+  @Test
+  public void TestInstrumentAndProfileAllSamples() {
+    Path samplesFolder = Path.of("sample");
+    File[] sampleFiles = samplesFolder.toFile().listFiles();
+    if (sampleFiles == null) {
+      throw new RuntimeException("no sample files found");
+    }
+    Stream.of(sampleFiles)
+        .filter(file -> !file.isDirectory())
+        .forEach(jFile -> {
+              String[] args = new String[]{
+                  samplesFolder.toString(),
+                  jFile.toString(),
+              };
+              Main.main(args);
+            }
+        );
+  }
+}
