8276513b8c5c4d242aaf1c046c73572ab0d0961d
Mathias Wöß <mat.woess@gmail.com>
2023-06-06 00:04:46 +0200

    add sample calling all other samples

---
+++ sample/AllSamples.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+
+public class AllSamples {
+  public static void main(String[] args) throws IOException {
+    Simple.main(args);
+    BasicElements.main(args);
+    Classes.main(args);
+    Enums.main(args);
+    Interfaces.main(args);
+    MissingBraces.main(args);
+    Packages.main(args);
+    Annotations.main(args);
+    Switches.main(args);
+    Lambdas.main(args);
+    AnonymousClasses.main(args);
+  }
+}
