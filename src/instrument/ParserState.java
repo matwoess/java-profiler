644c7b611df913000a1eb540d86ca8ca4c98f84a
Mathias Wöß <mat.woess@gmail.com>
2023-06-26 16:42:54 +0200

    improve grammar:  - treat yield like "return" (allow switch expression after keyword).

--- src/instrument/ParserState.java
+++ src/instrument/ParserState.java
@@ -228,6 +228,6 @@ public class ParserState {
   }
 
   boolean isAssignment() {
-    return parser.t.kind == _equals || parser.t.kind == _return;
+    return parser.t.kind == _equals || parser.t.kind == _return || parser.t.kind == _yield;
   }
 }
