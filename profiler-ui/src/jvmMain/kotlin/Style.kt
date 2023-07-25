23cb1ddad2228f6761bfb695d19828c23affa449
Mathias Wöß <mat.woess@gmail.com>
2023-07-25 16:07:50 +0200

    formatting

--- profiler-ui/src/jvmMain/kotlin/Style.kt
+++ profiler-ui/src/jvmMain/kotlin/Style.kt
@@ -7,33 +7,33 @@ import androidx.compose.ui.graphics.Color
 
 @Composable
 fun AppTheme(
-        darkTheme: Boolean = isSystemInDarkTheme(),
-        content: @Composable () -> Unit,
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit,
 ) {
     MaterialTheme(
-            colors = if (darkTheme) darkColors(
-                primary = Color(0xFFA4C9FF),
-                onPrimary = Color(0xFF00315D),
-                secondary = Color(0xFF50DBD0),
-                onSecondary = Color(0xFF003734),
-                background = Color(0xFF1A1C1E),
-                onBackground = Color(0xFFE3E2E6),
-                surface = Color(0xFF1A1C1E),
-                onSurface = Color(0xFFE3E2E6),
-                error = Color(0xFFFFB4AB),
-                onError = Color(0xFF690005),
-            ) else lightColors(
-                primary = Color(0xFF1960A5),
-                onPrimary = Color(0xFFFFFFFF),
-                secondary = Color(0xFF006A64),
-                onSecondary = Color(0xFFFFFFFF),
-                background = Color(0xFFFDFCFF),
-                onBackground = Color(0xFF1A1C1E),
-                surface = Color(0xFFFDFCFF),
-                onSurface = Color(0xFF1A1C1E),
-                error = Color(0xFFBA1A1A),
-                onError = Color(0xFFFFFFFF),
-            ),
-            content = content
+        colors = if (darkTheme) darkColors(
+            primary = Color(0xFFA4C9FF),
+            onPrimary = Color(0xFF00315D),
+            secondary = Color(0xFF50DBD0),
+            onSecondary = Color(0xFF003734),
+            background = Color(0xFF1A1C1E),
+            onBackground = Color(0xFFE3E2E6),
+            surface = Color(0xFF1A1C1E),
+            onSurface = Color(0xFFE3E2E6),
+            error = Color(0xFFFFB4AB),
+            onError = Color(0xFF690005),
+        ) else lightColors(
+            primary = Color(0xFF1960A5),
+            onPrimary = Color(0xFFFFFFFF),
+            secondary = Color(0xFF006A64),
+            onSecondary = Color(0xFFFFFFFF),
+            background = Color(0xFFFDFCFF),
+            onBackground = Color(0xFF1A1C1E),
+            surface = Color(0xFFFDFCFF),
+            onSurface = Color(0xFF1A1C1E),
+            error = Color(0xFFBA1A1A),
+            onError = Color(0xFFFFFFFF),
+        ),
+        content = content
     )
 }
