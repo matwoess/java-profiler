e3f362af7d9af44e6d7f0b9fe35ac39984e57a92
Mathias Wöß <mat.woess@gmail.com>
2023-07-29 00:42:46 +0200

    define visibilities by run mode

--- profiler-fxui/src/main/java/app/Controller.java
+++ profiler-fxui/src/main/java/app/Controller.java
@@ -1,19 +1,29 @@
 package app;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.HBox;
 
 public class Controller {
-  @FXML
-  private TextArea programOutput;
   @FXML
   private RadioButton rbDefaultMode;
   @FXML
   private RadioButton rbInstrumentOnly;
   @FXML
   private RadioButton rbReportOnly;
+  @FXML
+  private HBox hbMainFile;
+  @FXML
+  private HBox hbProgramArgs;
+  @FXML
+  private HBox hbSourcesDir;
+  @FXML
+  private CheckBox cbSyncCounters;
+  @FXML
+  private TextArea txtAreaOutput;
 
 
   @FXML
@@ -22,10 +32,15 @@ public class Controller {
     rbDefaultMode.setToggleGroup(toggleGroup);
     rbInstrumentOnly.setToggleGroup(toggleGroup);
     rbReportOnly.setToggleGroup(toggleGroup);
+    hbMainFile.visibleProperty().bind(rbReportOnly.selectedProperty().not());
+    hbProgramArgs.visibleProperty().bind(rbDefaultMode.selectedProperty());
+    hbSourcesDir.visibleProperty().bind(rbDefaultMode.selectedProperty());
+    cbSyncCounters.visibleProperty().bind(rbReportOnly.selectedProperty().not());
+
   }
 
   @FXML
   protected void onExecuteTool() {
-    programOutput.setText("Profiling now.");
+    txtAreaOutput.setText("Profiling now.\nHere's the output.");
   }
 }
