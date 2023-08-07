1c736754fda68c659c366d01f170605db7c45840
Mathias Wöß <mat.woess@gmail.com>
2023-08-08 00:55:53 +0200

    structure Controller init method into sub-methods

--- profiler-fxui/src/main/java/fxui/Controller.java
+++ profiler-fxui/src/main/java/fxui/Controller.java
@@ -7,20 +7,24 @@ import fxui.util.SystemOutputTextFlowWriter;
 import javafx.beans.binding.Binding;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-import javafx.scene.layout.HBox;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.text.TextFlow;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 
 import javax.swing.*;
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.file.Path;
 
-import java.awt.Desktop;
-
 public class Controller {
+  private static final Border invalidBorder = new Border(new BorderStroke(Color.RED, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT));
+  private static final Border validBorder = new Border(new BorderStroke(Color.GREEN, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT));
   @FXML
   private RadioButton rbDefaultMode;
   @FXML
@@ -68,49 +72,73 @@ public class Controller {
 
   @FXML
   private void initialize() {
-    {
-      rbDefaultMode.setToggleGroup(toggleGroup);
-      rbInstrumentOnly.setToggleGroup(toggleGroup);
-      rbReportOnly.setToggleGroup(toggleGroup);
-      rbDefaultMode.selectedProperty().addListener((observable, oldValue, newValue) -> {
-        if (newValue) parameters.runMode.set(RunMode.DEFAULT);
-      });
-      rbInstrumentOnly.selectedProperty().addListener((observable, oldValue, newValue) -> {
-        if (newValue) parameters.runMode.set(RunMode.INSTRUMENT_ONLY);
-      });
-      rbReportOnly.selectedProperty().addListener((observable, oldValue, newValue) -> {
-        if (newValue) parameters.runMode.set(RunMode.REPORT_ONLY);
-      });
-    }
-    {
-      hbMainFile.visibleProperty().bind(rbReportOnly.selectedProperty().not());
-      hbProgramArgs.visibleProperty().bind(rbDefaultMode.selectedProperty());
-      hbSourcesDir.visibleProperty().bind(rbDefaultMode.selectedProperty());
-      cbSyncCounters.visibleProperty().bind(rbReportOnly.selectedProperty().not());
-    }
-    {
-      btnMainFile.setOnAction(event -> chooseFile(txtMainFile));
-      btnSourcesDir.setOnAction(event -> chooseDirectory(txtSourcesDir));
-      btnOutputDir.setOnAction(event -> chooseDirectory(txtOutputDir));
-    }
-    {
-      txtMainFile.textProperty().bindBidirectional(parameters.mainFile);
-      txtProgramArgs.textProperty().bindBidirectional(parameters.programArgs);
-      txtSourcesDir.textProperty().bindBidirectional(parameters.sourcesDir);
-      txtOutputDir.textProperty().bindBidirectional(parameters.outputDir);
-      btnOpenReport.disableProperty().bindBidirectional(parameters.invalidOutDirPath);
-      Binding<Boolean> anyPathInvalid = parameters.invalidMainFilePath
-          .or(parameters.invalidSourcesDirPath)
-          .or(parameters.invalidOutDirPath);
-      btnRunTool.disableProperty().bind(anyPathInvalid);
-      cbVerboseOutput.selectedProperty().bindBidirectional(parameters.verboseOutput);
-      cbSyncCounters.selectedProperty().bindBidirectional(parameters.syncCounters);
-    }
-    {
-      PrintStream consoleOutput = new PrintStream(new SystemOutputTextFlowWriter(txtFlowOutput));
-      System.setOut(consoleOutput);
-      System.setErr(consoleOutput);
-    }
+    bindParameters();
+    setOnClickActions();
+    initConsoleOutput();
+    initRunModeControl();
+    initRunModeVisibilities();
+    initButtonDisabledProperties();
+    initBorderListeners();
+  }
+
+  private void bindParameters() {
+    txtMainFile.textProperty().bindBidirectional(parameters.mainFile);
+    txtProgramArgs.textProperty().bindBidirectional(parameters.programArgs);
+    txtSourcesDir.textProperty().bindBidirectional(parameters.sourcesDir);
+    txtOutputDir.textProperty().bindBidirectional(parameters.outputDir);
+    cbVerboseOutput.selectedProperty().bindBidirectional(parameters.verboseOutput);
+    cbSyncCounters.selectedProperty().bindBidirectional(parameters.syncCounters);
+  }
+
+  private void setOnClickActions() {
+    btnMainFile.setOnAction(event -> chooseFile(txtMainFile));
+    btnSourcesDir.setOnAction(event -> chooseDirectory(txtSourcesDir));
+    btnOutputDir.setOnAction(event -> chooseDirectory(txtOutputDir));
+  }
+
+  private void initRunModeControl() {
+    rbDefaultMode.setToggleGroup(toggleGroup);
+    rbInstrumentOnly.setToggleGroup(toggleGroup);
+    rbReportOnly.setToggleGroup(toggleGroup);
+    rbDefaultMode.selectedProperty().addListener((observable, oldValue, newValue) -> {
+      if (newValue) parameters.runMode.set(RunMode.DEFAULT);
+    });
+    rbInstrumentOnly.selectedProperty().addListener((observable, oldValue, newValue) -> {
+      if (newValue) parameters.runMode.set(RunMode.INSTRUMENT_ONLY);
+    });
+    rbReportOnly.selectedProperty().addListener((observable, oldValue, newValue) -> {
+      if (newValue) parameters.runMode.set(RunMode.REPORT_ONLY);
+    });
+  }
+
+  private void initConsoleOutput() {
+    PrintStream consoleOutput = new PrintStream(new SystemOutputTextFlowWriter(txtFlowOutput));
+    System.setOut(consoleOutput);
+    System.setErr(consoleOutput);
+  }
+
+  private void initRunModeVisibilities() {
+    hbMainFile.visibleProperty().bind(rbReportOnly.selectedProperty().not());
+    hbProgramArgs.visibleProperty().bind(rbDefaultMode.selectedProperty());
+    hbSourcesDir.visibleProperty().bind(rbDefaultMode.selectedProperty());
+    cbSyncCounters.visibleProperty().bind(rbReportOnly.selectedProperty().not());
+  }
+
+  private void initButtonDisabledProperties() {
+    btnOpenReport.disableProperty().bindBidirectional(parameters.invalidOutDirPath);
+    Binding<Boolean> anyPathInvalid = parameters.invalidMainFilePath
+        .or(parameters.invalidSourcesDirPath)
+        .or(parameters.invalidOutDirPath);
+    btnRunTool.disableProperty().bind(anyPathInvalid);
+  }
+
+  private void initBorderListeners() {
+    txtMainFile.textProperty().addListener((observable, oldValue, newValue)
+        -> txtMainFile.setBorder(parameters.invalidMainFilePath.get() ? invalidBorder : validBorder));
+    txtSourcesDir.textProperty().addListener((observable, oldValue, newValue)
+        -> txtMainFile.setBorder(parameters.invalidSourcesDirPath.get() ? invalidBorder : validBorder));
+    txtMainFile.textProperty().addListener((observable, oldValue, newValue)
+        -> txtMainFile.setBorder(parameters.invalidOutDirPath.get() ? invalidBorder : validBorder));
   }
 
   public void chooseFile(TextField pathField) {
