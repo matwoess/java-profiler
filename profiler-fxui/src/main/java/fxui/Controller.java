54a18b62079b74ff6f585ee80f450b066672a894
Mathias Wöß <mat.woess@gmail.com>
2023-08-18 23:43:23 +0200

    optimize imports

--- profiler-fxui/src/main/java/fxui/Controller.java
+++ profiler-fxui/src/main/java/fxui/Controller.java
@@ -6,14 +6,15 @@ import fxui.model.Parameters;
 import fxui.model.RunMode;
 import fxui.util.BindingUtils;
 import fxui.util.SystemUtils;
-import fxui.util.SystemOutputWriter;
 import javafx.beans.binding.BooleanBinding;
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
-import java.io.PrintStream;
 import java.nio.file.Path;
 
 public class Controller {
