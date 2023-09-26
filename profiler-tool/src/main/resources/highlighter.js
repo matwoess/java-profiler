2cef2cc3f479521b2473ba5b0b749a5c6210cd8b
Mathias Wöß <mat.woess@gmail.com>
2023-09-26 20:46:45 +0200

    optimize and improve highlighter.js code

--- profiler-tool/src/main/resources/highlighter.js
+++ profiler-tool/src/main/resources/highlighter.js
@@ -1,29 +1,33 @@
-var colorCovered = 'rgba(144, 238, 144, 0.3)';
-var colorNotCovered = 'rgba(255, 182, 193, 0.3)';
-var colorHoverHighlight = 'rgba(245, 222, 179, 1)';
+const colorCovered = 'rgba(144, 238, 144, 0.3)';
+const colorNotCovered = 'rgba(255, 182, 193, 0.3)';
+const colorHoverHighlight = 'rgba(245, 222, 179, 1)';
 
-var setCoverageBackground = function (span){
-	var backgroundColor = '';
-	if (span.hasClass('c')) {
-		backgroundColor = colorCovered;
-	} else if (span.hasClass('nc')) {
-		backgroundColor = colorNotCovered;
-	}
-	span.css({'background-color': backgroundColor})
+function setCoverageBackground(span) {
+  let backgroundColor = '';
+  if (span.hasClass('c')) {
+    backgroundColor = colorCovered;
+  } else if (span.hasClass('nc')) {
+    backgroundColor = colorNotCovered;
+  }
+  span.css({'background-color': backgroundColor})
 }
 
-$('span.c,span.nc').each(function () {setCoverageBackground($(this))});
-
-$("span").mouseenter(function (){
-		var classes = $(this).attr('class');
-		if (classes == null) return;
-		var classList = classes.split(/\s+/);
-		var lastClass = classList.slice(-1);
-		if (lastClass != null) {
-			$('span.'+lastClass).css({ 'background-color': colorHoverHighlight });
-		}
+$('span.c,span.nc').each(function () {
+  setCoverageBackground($(this))
 });
 
-$("span").mouseleave(function (){
-		$('span.c,span.nc').each(function () {setCoverageBackground($(this))});
+$("span").mouseenter(function () {
+  const classes = $(this).attr('class');
+  if (classes == null) return;
+  const classList = classes.split(/\s+/);
+  const lastClass = classList.slice(-1);
+  if (lastClass != null) {
+    $('span.' + lastClass).css({'background-color': colorHoverHighlight});
+  }
+});
+
+$("span").mouseleave(function () {
+  $('span.c,span.nc').each(function () {
+    setCoverageBackground($(this))
+  });
 });
