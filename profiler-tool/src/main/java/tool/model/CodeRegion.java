4cc51135491bef4c3e6c1c5642fefaff97ebba86
Mathias Wöß <mat.woess@gmail.com>
2023-10-08 03:37:00 +0200

    restore CodeRegion `toString`

--- profiler-tool/src/main/java/tool/model/CodeRegion.java
+++ profiler-tool/src/main/java/tool/model/CodeRegion.java
@@ -2,10 +2,19 @@ package tool.model;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CodeRegion implements Serializable {
   public CodePosition beg;
   public CodePosition end;
   public List<Block> minusBlocks = new ArrayList<>();
+
+  @Override
+  public String toString() {
+    return String.format(
+        "CodeRegion (beg=%s, end=%s, minusBlocks=%s)",
+        beg, end, Arrays.toString(minusBlocks.toArray())
+    );
+  }
 }
