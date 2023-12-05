987911d9b178770918d5f2a2c16f41436dc0baf2
Mathias Wöß <mat.woess@gmail.com>
2023-12-05 01:08:18 +0100

    update sample

--- sample/Fibonacci.java
+++ sample/Fibonacci.java
@@ -1,15 +1,13 @@
 class Fibonacci {
-
   static int fib(int n) {
     if (n <= 1)
       return n;
-    return fib(n - 1)
-        + fib(n - 2);
+    return fib(n - 1) + fib(n - 2);
   }
 
   public static void main(String[] args) {
     int N = Integer.parseInt(args[0]);
-    for (int i = 0; i < N; i++) {
+    for (int i = 1; i < N; i++) {
       System.out.print(fib(i) + " ");
     }
   }
