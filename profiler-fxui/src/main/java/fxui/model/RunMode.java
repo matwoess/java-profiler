fca985e3962987f147a1fac9220bd8af42e2f86d
Mathias Wöß <mat.woess@gmail.com>
2023-12-14 00:58:13 +0100

    - add JavaDoc to fxui.model{RunMode,Terminal}

--- profiler-fxui/src/main/java/fxui/model/RunMode.java
+++ profiler-fxui/src/main/java/fxui/model/RunMode.java
@@ -1,8 +1,14 @@
 package fxui.model;
 
+/**
+ * The possible run modes of the tool.
+ */
 public enum RunMode {
   DEFAULT, INSTRUMENT_ONLY, REPORT_ONLY;
 
+  /**
+   * {@return a description for the run mode}
+   */
   @Override
   public String toString() {
     return switch (this) {
