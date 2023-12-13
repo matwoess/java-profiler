951b473fc8de89786cf43c408f72c2c6cc63f5ec
Mathias Wöß <mat.woess@gmail.com>
2023-12-13 16:57:00 +0100

    update IO class documentation

--- profiler-common/src/main/java/common/IO.java
+++ profiler-common/src/main/java/common/IO.java
@@ -10,6 +10,15 @@ import java.util.stream.Stream;
 
 import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
+/**
+ * Helper class containing methods related to relative path finding and filesystem operations.
+ * <p>
+ * The {@link #outputDir} is the most important member variable.
+ * It specifies the root directory for all the tool's output files.
+ * No other paths are statically defined.
+ * All subdirectories and file locations can be retrieved by corresponding get methods,
+ * that resolve their file path relative to the <code>outputDir</code> member.
+ */
 public class IO {
   /**
    * Represents the output directory for the profiler.
@@ -23,106 +32,126 @@ public class IO {
   }
 
   /**
-   * @return the <code>instrumented<code> directory relative to the <code>outputDir</code>.
+   * Returns the <code>instrumented<code> directory relative to the {@link #outputDir}.
    */
   public static Path getInstrumentDir() {
     return getOutputDir().resolve("instrumented");
   }
 
   /**
-   * @return the <code>classes<code> directory relative to the <code>outputDir</code>.
+   * Returns the <code>classes<code> directory relative to the {@link #outputDir}.
    */
   public static Path getClassesDir() {
     return getOutputDir().resolve("classes");
   }
 
   /**
-   * The path where the instrumented version of a Java source file is stored.
-   * The tool will use this function to place instrumented copies in the same directory structure as from the sources
-   * directory, but relative to the instrumented directory.
+   * Returns the path where the instrumented version of a Java source file is stored.
+   * The tool will use this method to replicate the original directory structure for instrumented copies.
    *
-   * @param relativePath the source file path relative to the sources directory.
+   * @param relativePath the relative path of the original file to the sources directory.
    *                     Every <code>JavaFile</code> instance knows its relative location to the sources.
-   * @return the relative path of an instrumented copy according to the instrumented directory.
+   * @return the path of an instrumented copy relative to the instrumented directory
    */
   public static Path getInstrumentedFilePath(Path relativePath) {
     return getInstrumentDir().resolve(relativePath);
   }
 
   /**
-   * To successfully compile instrumented versions of source files, an import statement is added to all source files.
-   * A <code>__Counter.class</code> file is placed in an auxiliary package directory to resolve these imports.
+   * To successfully <b>compile</b> instrumented versions of source files, an import statement is added to all source files.
+   * A <code>__Counter.class</code> file is placed in an "auxiliary" package directory to resolve these imports.
    *
-   * @return where the pre-compiled <code>__Counter.class</code> class will be copied to for instrumentation.
+   * @return where the pre-compiled <code>__Counter.class</code> class will be copied to before compilation
    */
   public static Path getAuxiliaryCounterInstrumentPath() {
     return getInstrumentDir().resolve("auxiliary").resolve("__Counter.class");
   }
 
   /**
-   * To successfully run instrumented versions of source files, an import statement is added to all source files.
+   * To successfully <b>run</b> instrumented versions of source files, an import statement is added to all source files.
    * A <code>__Counter.class</code> file is placed in an auxiliary package directory to resolve this dependency.
    *
-   * @return where the pre-compiled <code>__Counter.class</code> class will be copied to for running the instrumented code.
+   * @return where the pre-compiled <code>__Counter.class</code> class will be copied to before running the instrumented code.
    */
   public static Path getAuxiliaryCounterClassPath() {
     return getClassesDir().resolve("auxiliary").resolve("__Counter.class");
   }
 
   /**
-   * @return the <code>metadata.dat<code> file relative to the <code>outputDir</code>.
+   * Returns the path to the parsed project's metadata file.
+   * This data is used for instrumentation and report generation.
+   *
+   * @return <code>metadata.dat<code> relative to the {@link #outputDir}
    */
   public static Path getMetadataPath() {
     return getOutputDir().resolve("metadata.dat");
   }
 
   /**
-   * @return the <code>counts.dat<code> file relative to the <code>outputDir</code>.
+   * Returns the path to the resulting counts file.
+   * This data is written as soon as the instrumented version is run and is used for report generation.
+   *
+   * @return <code>counts.dat<code> relative to the {@link #outputDir}
    */
   public static Path getCountsPath() {
     return getOutputDir().resolve("counts.dat");
   }
 
   /**
-   * @return the <code>report<code> directory relative to the <code>outputDir</code>.
+   * Returns the containing root directory for the generated report.
+   *
+   * @return <code>report<code> relative to the {@link #outputDir}
    */
   public static Path getReportDir() {
     return getOutputDir().resolve("report");
   }
 
   /**
-   * @return the class-index file path of the generated report.
+   * Returns the file path of the main report classes overview.
+   *
+   * @return <code>index.html</code> relative to the report directory
    */
   public static Path getReportIndexPath() {
     return getReportDir().resolve("index.html");
   }
 
   /**
-   * @return the path of the jQuery <code>highlighter.js</code> helper script to visualize blocks and regions in the report.
+   * Returns the path of the jQuery helper script to visualize blocks and regions in the report.
+   *
+   * @return <code>highlighter.js</code> relative to the report directory
    */
   public static Path getReportHighlighterPath() {
     return getReportDir().resolve("highlighter.js");
   }
 
   /**
-   * @return the path for the symlink to the main report index file.
+   * Returns the target path for the symlink pointing to the main report index file.
+   *
+   * @return <code>report.html</code> relative to the current directory
    */
   public static Path getReportIndexSymLinkPath() {
     return Path.of(".", "report.html");
   }
 
   /**
-   * @param clasName the name of the class. Will be appended as "index_" + <code>className</code>.
-   * @return the path to the methods-index for a class.
+   * Returns the path to the report method index of a java class.
+   * Every parsed class gets its own one during report generation.
+   *
+   * @param className the name of a parsed java class
+   * @return "index_" + <code>className</code> relative to the report directory
    */
-  public static Path getReportMethodIndexPath(String clasName) {
-    return IO.getReportDir().resolve("index_" + clasName + ".html");
+  public static Path getReportMethodIndexPath(String className) {
+    return IO.getReportDir().resolve("index_" + className + ".html");
   }
 
   /**
-   * @param relativePath the relative path from the <code>JavaFile</code> class relative to the sources directory.
-   *                     The ".java" suffix is replaced by ".html" to create the new file name.
-   * @return the location of an annotated report source code file.
+   * Returns the file path of an annotated HTML source file.
+   * The relative path of the original source file to its sources directory should be passed to the method.
+   * This way the package structure is replicated inside the <code>sources</code> report subdirectory.
+   * The ".java" suffix is replaced by ".html" to create the new file name.
+   *
+   * @param relativePath the relative path from a <code>JavaFile</code> class relative to its sources directory
+   * @return the location of the annotated report source code file
    */
   public static Path getReportSourceFilePath(Path relativePath) {
     Path reportFilePath = IO.getReportDir().resolve("source").resolve(relativePath);
@@ -130,21 +159,24 @@ public class IO {
   }
 
   /**
-   * @return the home directory of the current user.
+   * {@return the home directory of the current user}
    */
   public static Path getUserHomeDir() {
     return Path.of(System.getProperty("user.home"));
   }
 
   /**
-   * @return where the configured FxUI parameters are persisted to inside the project directory's <code>outDir</code>.
+   * Returns where the configured FxUI parameters are persisted to inside the output directory.
+   * @return <code>parameters.dat</code> relative to the {@link #outputDir}
    */
   public static Path getUIParametersPath() {
     return getOutputDir().resolve("parameters.dat");
   }
 
   /**
-   * @return location of the file for storing the previously opened project directory path.
+   * Returns the path of the file for storing the previously opened project directory.
+   * This is read when re-starting the FxUI application and pre-filled as the project to open.
+   * @return <code>lastProjectRootDirectory.txt</code> relative to the current directory
    */
   public static Path lastProjectPath() {
     return Path.of("lastProjectRootDirectory.txt");
@@ -157,9 +189,9 @@ public class IO {
   /**
    * Extract a copy of a resource file from the project directory or the .jar file.
    *
-   * @param resourceClass the anchor class used to locate the resource.
-   * @param resourceName  the name of the resource file to be extracted.
-   * @param destination   path where to copy the resource is copied to.
+   * @param resourceClass the anchor class used to locate the resource
+   * @param resourceName  the name of the resource file to be extracted
+   * @param destination   path where the resource is copied to
    */
   public static <T> void copyResource(Class<T> resourceClass, String resourceName, Path destination) {
     try (InputStream resource = resourceClass.getClassLoader().getResourceAsStream(resourceName)) {
@@ -178,8 +210,9 @@ public class IO {
    * Used to clean up output files before the next execution.
    * The root directory itself won't be removed to avoid locking errors.
    *
-   * @param directory the root directory that should be cleared of contents recursively.
-   */  public static void clearDirectoryContents(Path directory) {
+   * @param directory the root directory that should be cleared of contents recursively
+   */
+  public static void clearDirectoryContents(Path directory) {
     if (Files.exists(directory)) {
       try (Stream<Path> walk = Files.walk(directory)) {
         walk.sorted(Comparator.reverseOrder())
@@ -201,8 +234,8 @@ public class IO {
    * Creates all parent directories in the path description to ensure successful creation of a new file in it.
    *
    * @param fileOrFolder if a file is specified, all the parent directories will be created.
-   *                     If a directory is specified, all parent directories including the given path will be created.
-   * @return whether the operation was successful.
+   *                     If a directory is specified, all parent directories including the given one will be created.
+   * @return whether the operation was successful
    */
   @SuppressWarnings("UnusedReturnValue")
   public static boolean createDirectoriesIfNotExists(Path fileOrFolder) {
@@ -214,11 +247,11 @@ public class IO {
   }
 
   /**
-   * Creates a symbolic link at a given path location pointing to another path.
+   * Creates a symbolic link at a given path pointing to another file path.
    * Will work on Linux and macOS but might fail on Windows due to group policies.
    *
-   * @param link   the location of the linking file shortcut.
-   * @param target what the link should point to.
+   * @param link   the location of the linking file shortcut
+   * @param target what the link should point to
    */
   public static void createSymbolicLink(Path link, Path target) {
     try {
