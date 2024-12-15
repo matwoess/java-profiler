ca7f2943c6a378616551bcf9786206f9b5300e1e
Mathias Wöß <mat.woess@gmail.com>
2024-12-15 02:44:28 +0100

    update script to (temporarily) trust ssw site download blindly by --insecure flag

--- scripts/generate-parser.sh
+++ scripts/generate-parser.sh
@@ -4,6 +4,6 @@ project_dir="$(dirname "$0")/../"
 cd "$project_dir" || exit
 mkdir -p ./lib
 if [[ ! -f ./lib/Coco.jar ]]; then
-    curl --output lib/Coco.jar $coco_jar_url
+    curl --insecure --output lib/Coco.jar $coco_jar_url
 fi
 java -jar lib/Coco.jar -package tool.instrument java-profiler-tool/src/main/java/tool/instrument/JavaFile.atg
